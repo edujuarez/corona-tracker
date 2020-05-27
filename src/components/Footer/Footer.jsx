7f57751976f87ce5495dfb9ea68c50886d34d323
edujuarez <edukjuarez@gmail.com>
2020-05-27 15:00:44 -0300

    done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Styles from './Footer.module.css'
+
+const Footer = () => {
+
+
+return (
+    <div>
+        <p>Created by Kuki</p>
+        <a href= "https:\\linkedin.com/in/eduardo--juarez"> Contact</a>
+        <a href="https://github.com/edujuarez"> GitHub</a>
+    </div>
+)
+}
+
+export default Footer
